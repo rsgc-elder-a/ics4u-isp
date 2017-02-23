d063adcdb7ea6f6ea7a1792da0b2abb01e2d5c84
Andrew <aelder@rsgc.on.ca>
2017-02-22 20:22:23 -0500

    Did not attach all docs.

---
+++ BinMatch/BinMatch/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  BinMatch
+//
+//  Created by Andy-E on 2017-02-22.
+//  Copyright © 2017 Andy-E. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+        
+        
+        
+    }
+    
+    
+
+}
+
